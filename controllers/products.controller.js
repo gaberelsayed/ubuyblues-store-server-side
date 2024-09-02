c746a25c4ce8b48cde28650c40420b10622e7c90
anas derk <anas.derk2020@gmail.com>
2024-09-03 01:40:27 +0300

    Continue

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -210,6 +210,7 @@ async function deleteImageFromProductGallery(req, res) {
         res.json(result);
     }
     catch (err) {
+        console.log(err);
         res.status(500).json(getResponseObject("Internal Server Error !!", true, {}));
     }
 }
